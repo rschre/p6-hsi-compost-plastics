acbae713a070fb4bb55d8f54c251c59deca42f65
Raffael Schreiber <49059499+rschre@users.noreply.github.com>
2022-08-13 13:09:58 +0200

    added compare preprocessing

--- data_handling.py
+++ data_handling.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import spectral
 from matplotlib.patches import Rectangle
+from matplotlib.widgets import Slider
 from scipy.signal import savgol_filter
 
 logging.basicConfig(level=logging.INFO)
@@ -51,28 +52,7 @@ def get_hsi_capture(
         return None
 
 
-def get_root_folder(device: str = None):
-
-    root_pc = r"D:\OneDriveFHNW\FHNW\EUT-P6bb-21HS-RS_M365 - General\captures"
-    root_notebook = r"C:\Users\Neu\FHNW\EUT-P6bb-21HS-RS_M365 - General\captures"
-
-    if device == "PC":
-        root = root_pc
-    elif device == "NB":
-        root = root_notebook
-    else:
-        select_root = input("Notebook / Home-PC? [NB/PC]")
-        if select_root.upper() == "NB":
-            root = root_notebook
-        elif select_root.upper() == "PC":
-            root = root_pc
-        else:
-            root = input("Enter alternative path for root:\n")
-
-    logger.info(f"Root folder set to {root}")
-    return root
-
-
+# flips the image horizontally so it's aligned like in real life
 def load_hsi_data(capture):
     return np.flip(capture.load(), axis=1)
 
@@ -107,7 +87,12 @@ def get_2d_roi(
     return data_2d
 
 
+## here for backwards comatibility, but limit_reflectance makes more sense
 def limit_reflection(data, threshold=8500):
+    return np.where(data > threshold, threshold, data)
+
+
+def limit_reflecance(data, threshold=8500):
     return np.where(data > threshold, threshold, data)
 
 
@@ -145,11 +130,6 @@ def display_roi_rectangle(
     return view_roi
 
 
-def get_mean_plot(hsi_data_2d, column_names, title="Mean Spectrum", labels=None):
-    df = pd.DataFrame(hsi_data_2d, columns=column_names)
-    return df.mean().plot(labels=labels, title=title)
-
-
 def snv_transform(input_data):
     """
     :snv: Standard Normal Variate Transformation:
@@ -184,41 +164,6 @@ def snv_transform(input_data):
         return data_snv
 
 
-def smooth_and_transform(
-    data, smoothing_window_length=15, smoothing_polyorder=3, band_low=8, band_high=210
-):
-    """Smoothing the data using a Savitzy-Golay Filter and transforming the data to the range [0,1]
-    Parameters
-    ----------
-    data : ndarray
-        The data to be smoothed and transformed.
-    smoothing_window_length : int
-        The length of the smoothing window.
-    smoothing_polyorder : int
-        The order of the smoothing polynomial.
-    band_low : int
-        The lower bound of the band to be transformed.
-    band_high : int
-        The upper bound of the band to be transformed.
-    Returns
-    -------
-    ndarray
-        The smoothed and transformed data.
-    """
-
-    column_names = np.round(np.linspace(900, 1700, 224), 1)[band_low:band_high]
-    data_smooth = savgol_filter(
-        data,
-        window_length=smoothing_window_length,
-        polyorder=smoothing_polyorder,
-        axis=1,
-    )
-    data_snv = snv_transform(data_smooth)
-    data_df = pd.DataFrame(data_snv, columns=column_names)
-
-    return data_df
-
-
 def remove_outliers(hsi_data_2d, z_score_threshold=3, summary=False):
     """
     :param data_df: Dataframe of which outliers will be removed. pixels as rows, wavelength as columns
@@ -240,8 +185,76 @@ def remove_outliers(hsi_data_2d, z_score_threshold=3, summary=False):
     return hsi_data_2d.drop(outliers)
 
 
+def get_variable_savgol_plot(data: pd.DataFrame):
+    """Takes a pandas dataframe in which the column names are the values for the x-axis
+    (usually wavelengths) and the rows of the dataframe are spectras. The spectra and
+    the spectra transformed using Savitzky-Golay-Filter are plotted. The plot displays a slider,
+    which allows to adjust the polyorder and window_length of the savgol filter.
+
+    :param data: a pandas dataframe containing spectral data
+    """
+
+    # Initial x and y arrays
+    x = data.columns.astype("float64")
+    y = np.squeeze(data.values)
+    lines = []
+    lines_savgol = []
+
+    # Plotting
+    fig = plt.figure()
+    plt.subplots_adjust(bottom=0.25)
+    ax = fig.subplots()
+    plt.title("Variabler Savitzky-Golay-Filter")
+
+    # Create a line for each row of the dataframe
+    for i in range(0, len(y)):
+        lines.append(ax.plot(x, y[i]))
+        y_savgol = savgol_filter(y[i], window_length=7, polyorder=3)
+        lines_savgol.append(ax.plot(x, y_savgol, "g"))
+
+    # update method, is called when an adjustment is made to one of the sliders
+    def update_sliders(val):
+        # prevents polyorder >= window_length
+        if slider_window.val <= slider_polyorder.val:
+            slider_window.eventson = False
+            slider_window.set_val((slider_polyorder.val + 1) // 2 * 2 + 1)
+            slider_window.eventson = True
+
+        sliders = [slider_window.val, slider_polyorder.val]
+
+        # draws the transformed spectra
+        for i in range(0, len(y)):
+            lines_savgol[i][0].set_ydata(
+                savgol_filter(y[i], window_length=sliders[0], polyorder=sliders[1])
+            )
+        # redrawing the figure
+        fig.canvas.draw()
+
+    # Window length slider xposition, yposition, width and height
+    ax_slider = plt.axes([0.35, 0.1, 0.55, 0.03])
+    # Properties of the slider
+    slider_window = Slider(
+        ax_slider, "Savgol-Filter Window Length", 5, 50, valinit=7, valstep=2
+    )
+    slider_window.on_changed(update_sliders)
+
+    # Polyorder slider xposition, yposition, width and height
+    ax_slider_poly = plt.axes([0.35, 0.15, 0.55, 0.03])
+    # Properties of the slider
+    slider_polyorder = Slider(
+        ax_slider_poly, "Savgol-Filter Polyorder", 2, 10, valinit=3, valstep=1
+    )
+    slider_polyorder.on_changed(update_sliders)
+
+    plt.show()
+
+    return (slider_polyorder, slider_window)
+
+
 def get_sample_plot(hsi_data: pd.DataFrame, sample_count: int, title=None, labels=None):
     """
+    Returns a plotly plot of a specified numbers of samples of the data
+
     :param hsi_data: Dataframe of which a sample will be taken
     :param sample_count: Number of samples to be taken
 
@@ -266,7 +279,7 @@ def msc_transform(input_data: np.array, reference: np.array = None):
     :returns: data_msc (np.array): data transformed by msc
     """
 
-    # Set type to float
+    # Set type to float (also transforms if a df instead of array is provided)
     input_data = np.array(input_data, dtype=np.float64)
 
     # If reference is provided save it to the variable ref, otherwise use mean of dataset
@@ -280,8 +293,36 @@ def msc_transform(input_data: np.array, reference: np.array = None):
     data_msc = np.zeros_like(input_data)
     for i in range(input_data.shape[0]):
         # Least Squares Regression
-        fit = np.polyfit(reference_data[i], input_data[i, :], 1, full=True)
+        fit = np.polyfit(reference_data, input_data[i, :], 1, full=True)
         # Apply correction to data
         data_msc[i, :] = (input_data[i, :] - fit[0][1]) / fit[0][0]
 
     return data_msc
+
+
+def get_mean_plot(hsi_data_2d, column_names, title="Mean Spectrum", labels=None):
+    df = pd.DataFrame(hsi_data_2d, columns=column_names)
+    return df.mean().plot(labels=labels, title=title)
+
+
+# teams syncs to different folders on notebook and pc, not having to type the path every time
+def get_root_folder(device: str = None):
+
+    root_pc = r"D:\OneDriveFHNW\FHNW\EUT-P6bb-21HS-RS_M365 - General\captures"
+    root_notebook = r"C:\Users\Neu\FHNW\EUT-P6bb-21HS-RS_M365 - General\captures"
+
+    if device == "PC":
+        root = root_pc
+    elif device == "NB":
+        root = root_notebook
+    else:
+        select_root = input("Notebook / Home-PC? [NB/PC]")
+        if select_root.upper() == "NB":
+            root = root_notebook
+        elif select_root.upper() == "PC":
+            root = root_pc
+        else:
+            root = input("Enter alternative path for root:\n")
+
+    logger.info(f"Root folder set to {root}")
+    return root
